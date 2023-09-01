27ab1f133aa4aa92c6b3bde097876b3bbdd61fd8
Claptrap-maker <lelik575435@gmail.com>
2023-09-02 00:05:54 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,34 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator <T> implements Iterator<T> {
+
+    private List<T> data;
+    CyclicIterator<T> cyclicIterator;
+    Iterator<T> iterator;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+        iterator = data.iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (!iterator.hasNext()) {
+            cyclicIterator = new CyclicIterator<>(data);
+            iterator = cyclicIterator.iterator;
+        }
+        return iterator.hasNext();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return iterator.next();
+    }
+}
