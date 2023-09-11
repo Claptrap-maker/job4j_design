219bbb251fc07ae80c1a1b28b39b0b20dbd9abfd
Claptrap-maker <lelik575435@gmail.com>
2023-09-11 14:52:17 +0300

    5. Очередь на двух стеках [#160 #444101]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,37 +5,29 @@ import java.util.NoSuchElementException;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    boolean inHasChanged;
     private int inSize;
     private int outSize;
 
     public T poll() {
-        if (inHasChanged) {
-            reverseStack();
-            inHasChanged = false;
-        }
-        if (outSize == 0) {
+        if (inSize == 0 && outSize == 0) {
             throw new NoSuchElementException("Queue is empty");
+        }
+        if (inSize != outSize) {
+            for (int i = 0; i < outSize; i++) {
+                in.push(out.pop());
+            }
+            for (int i = 0; i < inSize; i++) {
+                out.push(in.pop());
+            }
+            outSize = inSize;
+            inSize = 0;
         }
         outSize--;
         return out.pop();
     }
 
-    private void reverseStack() {
-        for (int i = 0; i < outSize; i++) {
-            in.push(out.pop());
-        }
-        outSize = 0;
-        for (int i = 0; i < inSize; i++) {
-            out.push(in.pop());
-            outSize++;
-        }
-        inSize = 0;
-    }
-
     public void push(T value) {
         in.push(value);
         inSize++;
-        inHasChanged = true;
     }
 }
