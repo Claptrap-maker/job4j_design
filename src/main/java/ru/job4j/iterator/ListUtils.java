9f05799166bf9c64b15333a1f0a3771e6d132190
Claptrap-maker <lelik575435@gmail.com>
2024-01-21 23:37:08 +0300

    7. ListIterator (написала в одну строчку)

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -9,11 +9,13 @@ public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         list.listIterator(index).add(value);
+
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         list.listIterator(index + 1).add(value);
+
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
@@ -39,6 +41,7 @@ public class ListUtils {
     public static <T> void removeAll(List<T> list, List<T> elements) {
         if (elements != null && !elements.isEmpty()) {
             removeIf(list, elements::contains);
+
         }
     }
 }
