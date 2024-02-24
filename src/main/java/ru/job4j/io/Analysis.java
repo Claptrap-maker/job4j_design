9b0c9f2a00678a0e0b0fe70921b0ebd421ef187e
Claptrap-maker <lelik575435@gmail.com>
2024-02-24 19:54:39 +0300

    7. Анализ доступности сервера

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -4,34 +4,25 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.function.BiFunction;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        StringBuilder builder = new StringBuilder();
-        BiFunction<String, StringBuilder, StringBuilder> function = (s, sb) -> {
-            StringBuilder result = new StringBuilder();
-            String[] buff = s.split(" ");
-            int size = sb.toString().split(";").length;
-            if (size % 2 != 0 && size != 1) {
-                sb.setLength(0);
-            }
-            if (("400".equals(buff[0]) || "500".equals(buff[0])) && "".equals(sb.toString())) {
-                sb.append(buff[1]);
-                sb.append(";");
-            }
-            if (!"".equals(sb.toString()) && ("200".equals(buff[0]) || "300".equals(buff[0]))) {
-                sb.append(buff[1]);
-                sb.append(";\n");
-                result = sb;
-            }
-            return result;
-        };
+        AtomicBoolean flag = new AtomicBoolean(false);
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(target)) {
             writer.println(target.split("\\.")[1]);
             reader.lines()
-                    .forEachOrdered(line -> writer.print(function.apply(line, builder)));
+                    .forEachOrdered(line -> {
+                        String[] buff = line.split(" ");
+                        if (("400".equals(buff[0]) || "500".equals(buff[0])) && !flag.get()) {
+                            writer.print(buff[1] + ";");
+                            flag.set(true);
+                        } else if (("200".equals(buff[0]) || "300".equals(buff[0])) && flag.get()) {
+                            writer.print(buff[1] + ";\n");
+                            flag.set(false);
+                        }
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
