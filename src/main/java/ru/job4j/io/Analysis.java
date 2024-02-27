d7895551fd6bcb06ca63d7e71d35f6e4353e5bcc
Claptrap-maker <lelik575435@gmail.com>
2024-02-27 16:33:31 +0300

    7. Анализ доступности сервера (упростила unavailable())

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -4,25 +4,23 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        AtomicBoolean flag = new AtomicBoolean(false);
+        boolean flag = false;
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(target)) {
             writer.println(target.split("\\.")[1]);
-            reader.lines()
-                    .forEachOrdered(line -> {
-                        String[] buff = line.split(" ");
-                        if (("400".equals(buff[0]) || "500".equals(buff[0])) && !flag.get()) {
-                            writer.print(buff[1] + ";");
-                            flag.set(true);
-                        } else if (("200".equals(buff[0]) || "300".equals(buff[0])) && flag.get()) {
-                            writer.print(buff[1] + ";\n");
-                            flag.set(false);
-                        }
-                    });
+            while (reader.ready()) {
+                String s = reader.readLine();
+                String[] buff = s.split(" ");
+                if ((!flag && ("400".equals(buff[0]) || "500".equals(buff[0])))
+                        || (flag && ("200".equals(buff[0]) || "300".equals(buff[0])))) {
+                    String separator = !flag ? ";" : ";\n";
+                    writer.print(buff[1] + separator);
+                    flag = !flag;
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
