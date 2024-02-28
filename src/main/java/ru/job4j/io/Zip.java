39be55e2a2c2a8b0f0955b15c6b4b277839f689a
Claptrap-maker <lelik575435@gmail.com>
2024-02-28 16:53:16 +0300

    14. Архивировать проект [#861 #483797]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -23,17 +23,6 @@ public class Zip {
 
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream output = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(output.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
         ArgsName argsName = ArgsName.of(args);
         List<Path> paths = new ArrayList<>();
